5cbb007cb5791987acbff59c6c8575cf67264dff
jainambharvad9 <jainambharvad9@gmail.com>
2025-12-31 18:01:44 +0530

    changes on @vite CSS NPM

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
     <!-- LMS Styles -->
-    @vite('resources/css/lms.css')
+    {{-- @vite('resources/css/lms.css') --}}
 
+    <link rel="stylesheet" href="{{ asset('css/lms.css') }}">
     @stack('styles')
 </head>
 
